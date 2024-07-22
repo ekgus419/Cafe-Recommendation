758fdb125288e49f154e190ad1acd21421677515
ekgus419 <ekgus419@gmail.com>
2024-07-22 16:16:57 +0900

    #1-init-dev
    기본 개발 환경을 구축한다.

---
+++ src/main/java/cafe/recommendation/CafeRecommendationApplication.java
@@ -0,0 +1,13 @@
+package cafe.recommendation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeRecommendationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeRecommendationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cafe/recommendation/CafeRecommendationApplicationTests.java
@@ -0,0 +1,13 @@
+package cafe.recommendation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeRecommendationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
